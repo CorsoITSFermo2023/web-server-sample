6db600f9962f3fe77135c0708b48f0c4a99d18c4
Brian <cobyb1211@gmail.com>
2023-05-22 18:15:05 +0200

    asd

--- dao.js
+++ dao.js
@@ -1,4 +1,4 @@
-const { all, insert, remove } = require("./db")
+const { all, insert, remove, update, get } = require("./db")
 
 /**
  * 
--- express.js
+++ express.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const { initStruct } = require('./init-struct');
-const { listaProdotti, insertProdotto, updateProdotto } = require('./dao');
+const { listaProdotti, insertProdotto, updateProdotto, getProdotto} = require('./dao');
+const { update, get } = require('./db');
 const port = 3000;
 
 const app = express();
@@ -47,20 +48,23 @@ app.get('/list', async (req, res) => {
   ); */
 })
 
-app.get('/:idProdotto', (req, res) => {
-  req.params.idProdotto
+app.get('/:idProdotto', async (req, res) => {
+  const id = await (req.params.idProdotto);
   // TODO fare la logica
+  const prodotto = await getProdotto(id);
+  res.json(prodotto)
 })
 
 app.post('/', async (req, res) => {
   const newId = await insertProdotto(req.body.descrizione, req.body.price, req.body.dettagli)
   res.json(newId);
-});
+}); 
 
 app.put('/:idProdotto', async (req, res) => {
   const idProdotto = req.params.idProdotto;
   const {descrizione, price, dettagli} = req.body;
   const result = await updateProdotto(idProdotto, descrizione, price, dettagli)
+  res.json(result);
 
 });
 
@@ -71,4 +75,4 @@ app.delete('/:idProdotto', (req, res) => {
 
 initStruct().then(
   () => app.listen(port)
-);
+); 
